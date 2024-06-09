0a4a996185e1f79477141821c144bc1a388068d9
Lovlam <duongbaolam2214@gmail.com>
2024-06-10 02:39:51 +0700

    Be Xuan Mai

--- src/main/java/com/hotelbooking/hotelbooking/Service/AccountService.java
+++ src/main/java/com/hotelbooking/hotelbooking/Service/AccountService.java
@@ -5,19 +5,13 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.hotelbooking.hotelbooking.Entity.Account;
 import com.hotelbooking.hotelbooking.Repository.AccountRepo;
 
 @Service
-public class AccountService implements UserDetailsService {
+public class AccountService {
     @Autowired
     private AccountRepo accountRepo;
 
@@ -31,21 +25,4 @@ public class AccountService implements UserDetailsService {
     }
 
 
-    @Override
-    public UserDetails loadUserByUsername(String phoneNumber) throws UsernameNotFoundException {
-        Account account = accountRepo.findAccountByPhoneNumber(phoneNumber);
-
-        if (account == null) {
-            throw new UsernameNotFoundException("Account not found");
-        }
-
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        if (account.isRole()) {
-            authorities.add(new SimpleGrantedAuthority("ADMIN"));
-        } else {
-            authorities.add(new SimpleGrantedAuthority("USER"));
-        }
-
-        return new User(account.getPhoneNumber(), account.getPassword(), authorities);
-    }
 }
